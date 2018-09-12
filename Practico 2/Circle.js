7809167d1f87a62ada5861827b0f3c6380ec7bd4
Maximiliano Guerra <guerramdg@gmail.com>
2018-09-12 19:34:16 -0300

    trabajo en clase

--- Practico 2/Circle.js
+++ Practico 2/Circle.js
@@ -19,4 +19,43 @@ class Circle {
     ctx.fill();
     ctx.closePath();
   }
+  dibujarConImagen(image,ctx){
+    ctx.beginPath();
+    ctx.fillStyle = image;
+    ctx.arc(this.posX,this.posY,this.radio,0,Math.PI*2)
+    ctx.fill();
+    ctx.closePath();
+  }
+  dibujarConGradiente(){
+    let gradient = ctx.createLinearGradient(this.posX-this.radio,this.posY,this.posX+this.radio,this.posY);
+    gradient.addColorStop(0,'black')
+    gradient.addColorStop(1/2,'yellow')
+    gradient.addColorStop(1,'red')
+    ctx.beginPath();
+    ctx.fillStyle = gradient;
+    ctx.arc(this.posX,this.posY,this.radio,0,Math.PI*2)
+    ctx.fill();
+    ctx.closePath();
+  }
+  dibujarConGradienteRadial(){
+    let gradient = ctx.createRadialGradient(this.posX,this.posY,this.radio/8,this.posX,this.posY,this.radio);
+    gradient.addColorStop(0,'black')
+    gradient.addColorStop(1/2,'yellow')
+    gradient.addColorStop(1,'red')
+    ctx.beginPath();
+    ctx.font = "bold 22px sans-serif";
+    ctx.fillText("Texto en el Canvas",this.posX-this.radio,this.posY-(this.radio+10));
+    ctx.fillStyle = gradient;
+    ctx.arc(this.posX,this.posY,this.radio,0,Math.PI*2)
+    ctx.fill();
+    ctx.closePath();
+  }
+  isCkicked(x,y){
+    let d=Math.Sqrt(Math.Pow(this.posX-x)+Math.Pow(this.posY-y))
+    if(this.radio>d){
+      return true;
+    }
+    return false;
+  }
+
 }
--- Practico 2/Main.js
+++ Practico 2/Main.js
@@ -1,12 +1,28 @@
+let ctx = document.getElementById('canvas').getContext('2d');
+
+let image1 = new Image();
+image1.src="tiburon-final.jpg"
 let circulo = new Circle();
 let circulo2 = new Circle();
+let circulo3 = new Circle();
+let circulo4 = new Circle();
+let circulo5 = new Circle();
 let rectangulo = new Rectangle();
 let rectangulo2 = new Rectangle();
 let triangle = new Triangle();
 circulo2.setValues(500,500,40,'#89AC76');
+circulo3.setValues(600,600,50,'#89AC76');
+circulo4.setValues(600,200,150,'#89AC76');
+circulo5.setValues(100,500,100,'#89AC76')
 rectangulo2.setValues(100,100,40,40,'#89AC76');
 circulo.dibujar();
 circulo2.dibujar();
 rectangulo.dibujar();
 rectangulo2.dibujar();
 triangle.dibujar();
+image1.onload = function () {
+  let image = ctx.createPattern(this , "repeat")
+  circulo3.dibujarConImagen(image,ctx);
+}
+circulo4.dibujarConGradiente();
+circulo5.dibujarConGradienteRadial();
